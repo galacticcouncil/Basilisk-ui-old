853b042792999df74d8b90404bda9b0280966c4d
Matej Sima <matej.sima@stove-labs.com>
2021-10-05 14:59:47 +0200

    build: configured storybook & formatjs (react-intl)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl';
 
-function App() {
+export enum Locale {
+  EN = 'en'
+}
+export interface AppProps {
+  locale: Locale
+}
+
+function App({ locale }: AppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider
+      messages={{}}
+      locale={locale}
+      defaultLocale='en'
+    >
+      <p>
+        <FormattedMessage
+          id='greeting'
+          description='Application wide greeting'
+          defaultMessage='Today is {ts, date, ::yyyyMMdd}'
+          values={{
+            ts: Date.now()
+          }}
+        />
+
+        <br/>
+
+        <FormattedNumber
+          value={19}
+          style='currency'
+          currency='EUR'
+        />
+      </p>
+    </IntlProvider>
   );
 }
 
