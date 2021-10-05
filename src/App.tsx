ed509804936a628d7ae273612e88eda841fdc904
Matej Sima <matej.sima@stove-labs.com>
2021-10-05 15:12:50 +0200

    style(bootstrap): added the sass version of bootstrap to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl';
+import './App.scss';
+import { IntlProvider, FormattedMessage, FormattedNumber } from 'react-intl';
 
 export enum Locale {
   EN = 'en'
@@ -12,30 +12,32 @@ export interface AppProps {
 
 function App({ locale }: AppProps) {
   return (
-    <IntlProvider
-      messages={{}}
-      locale={locale}
-      defaultLocale='en'
-    >
-      <p>
-        <FormattedMessage
-          id='greeting'
-          description='Application wide greeting'
-          defaultMessage='Today is {ts, date, ::yyyyMMdd}'
-          values={{
-            ts: Date.now()
-          }}
-        />
+    <div className="app text-center">
+      <IntlProvider
+        messages={{}}
+        locale={locale}
+        defaultLocale='en'
+      >
+        <p>
+          <FormattedMessage
+            id='greeting'
+            description='Application wide greeting'
+            defaultMessage='Today is {ts, date, ::yyyyMMdd}'
+            values={{
+              ts: Date.now()
+            }}
+          />
 
-        <br/>
+          <br />
 
-        <FormattedNumber
-          value={19}
-          style='currency'
-          currency='EUR'
-        />
-      </p>
-    </IntlProvider>
+          <FormattedNumber
+            value={19}
+            style='currency'
+            currency='EUR'
+          />
+        </p>
+      </IntlProvider>
+    </div>
   );
 }
 
