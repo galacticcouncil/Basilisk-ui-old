ef2771a3a0ec9dad9b4eeaa922e7ab62921b61fd
Václav Slavík <vaclav.slavik@topmonks.com>
2021-12-20 16:31:20 +0100

    reintroduce bootstrap 5 - use it's flex classes

--- src/components/Wallet/Wallet.stories.tsx
+++ src/components/Wallet/Wallet.stories.tsx
@@ -2,54 +2,60 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { useRef } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 
-import cssColors from './../../misc/colors.module.scss'
+import cssColors from './../../misc/colors.module.scss';
 import { StorybookWrapper } from '../../misc/StorybookWrapper';
 
-import { Wallet } from "./Wallet";
-import { toPrecision12 } from "../../hooks/math/useToPrecision";
+import { Wallet } from './Wallet';
+import { toPrecision12 } from '../../hooks/math/useToPrecision';
 
 export default {
-    title: 'components/Wallet',
-    component: Wallet,
-    args: {
-        account: {
-            name: 'Alice 1',
-            balances: [{ assetId: '0', balance: toPrecision12('100200') }],
-            id: '1',
-            isActive: true,
-            vestingSchedule: {}
-        }
-    }
-} as ComponentMeta<typeof Wallet>
+  title: 'components/Wallet',
+  component: Wallet,
+  args: {
+    account: {
+      name: 'Alice 1',
+      balances: [{ assetId: '0', balance: toPrecision12('100200') }],
+      id: 'E7ncQKp4xayUoUdpraxBjT7NzLoayLJA4TuPcKKboBkJ5GH',
+      isActive: true,
+      vestingSchedule: {},
+    },
+  },
+} as ComponentMeta<typeof Wallet>;
 
 const Template: ComponentStory<typeof Wallet> = (args) => {
-    const modalContainerRef = useRef<HTMLDivElement | null>(null);
-    const methods = useForm();
+  const modalContainerRef = useRef<HTMLDivElement | null>(null);
+  const methods = useForm();
 
-    return <StorybookWrapper>
-        <div style={{
-            margin: '-1rem',
-            padding: '1rem',
-            backgroundColor: cssColors.gray2
-        }} >
-            {/* This is where the underlying modal should be rendered */}
-            <div ref={modalContainerRef} />
+  return (
+    <StorybookWrapper>
+      <div
+        style={{
+          margin: '-1rem',
+          padding: '1rem',
+          backgroundColor: cssColors.gray2,
+        }}
+      >
+        {/* This is where the underlying modal should be rendered */}
+        <div ref={modalContainerRef} />
 
-            <FormProvider {...methods}>
-                <form onSubmit={methods.handleSubmit(() => {})}>
-                    {/*
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(() => {})}>
+            {/*
                         Pass the ref to the element above, so that the Wallet
                         can render the modal there.
                     */}
-                    <div style={{
-                        width: '360px'
-                    }}>
-                        <Wallet {...args} modalContainerRef={modalContainerRef}/>
-                    </div>
-                </form>
-            </FormProvider>
-        </div>
+            <div
+              style={{
+                width: '360px',
+              }}
+            >
+              <Wallet {...args} modalContainerRef={modalContainerRef} />
+            </div>
+          </form>
+        </FormProvider>
+      </div>
     </StorybookWrapper>
-}
+  );
+};
 
 export const Default = Template.bind({});
--- src/misc/StorybookWrapper.tsx
+++ src/misc/StorybookWrapper.tsx
@@ -1,14 +1,12 @@
-import { IntlProvider } from "react-intl"
-import { Locale } from "./locale"
-import enMessages from '../lang/en.json'
+import { IntlProvider } from 'react-intl';
+import { Locale } from './locale';
+import './../index.scss';
 import './shared';
 
-export const StorybookWrapper = ({children}: any) => {
-    return <IntlProvider
-        messages={{}}
-        locale={Locale.EN}
-        defaultLocale='en'
-    >
-        {children}
+export const StorybookWrapper = ({ children }: any) => {
+  return (
+    <IntlProvider messages={{}} locale={Locale.EN} defaultLocale="en">
+      {children}
     </IntlProvider>
-}
+  );
+};
