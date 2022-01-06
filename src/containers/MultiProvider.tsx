b70902e3d1c691c38ac0202cb57668fb74366202
Václav Slavík <vaclav.slavik@topmonks.com>
2022-01-06 16:52:33 +0100

    use useApollo rather than useConfigureApolloClient

--- src/containers/MultiProvider.tsx
+++ src/containers/MultiProvider.tsx
@@ -1,41 +1,35 @@
-import { ApolloProvider, useApolloClient } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
-import { GetActiveAccountQueryProvider } from '../hooks/accounts/queries/useGetActiveAccountQuery';
-import { useConfigureApolloClient } from '../hooks/apollo/useApollo';
+import { useApollo } from '../hooks/apollo/useApollo';
 import { LastBlockProvider } from '../hooks/lastBlock/useSubscribeNewBlockNumber';
 import { GetExtensionQueryProvider } from '../hooks/polkadotJs/useGetExtensionQuery';
 import { PolkadotJsProvider } from '../hooks/polkadotJs/usePolkadotJs';
 import { MathProvider } from '../hooks/math/useMath';
 
-export const ConfiguredApolloProvider = ({ children }: { children: React.ReactNode }) => {
-    const client = useConfigureApolloClient();
-    return (
-        <ApolloProvider client={client}>
-            {children}
-        </ApolloProvider>
-    )
-}
+export const ConfiguredApolloProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const client = useApollo();
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
 
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => (
-    <GetExtensionQueryProvider>
-        {children}
-    </GetExtensionQueryProvider>
-)
+  <GetExtensionQueryProvider>{children}</GetExtensionQueryProvider>
+);
 
 // TODO: use react-multi-provider instead of ugly nesting
 export const MultiProvider = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <PolkadotJsProvider>
-            <MathProvider>
-                <LastBlockProvider>
-                    <ConfiguredApolloProvider>
-                        <QueryProvider>
-                            {children}
-                        </QueryProvider>
-                    </ConfiguredApolloProvider>
-                </LastBlockProvider>
-            </MathProvider>
-        </PolkadotJsProvider>
-
-    )
-}
+  return (
+    <PolkadotJsProvider>
+      <MathProvider>
+        <LastBlockProvider>
+          <ConfiguredApolloProvider>
+            <QueryProvider>{children}</QueryProvider>
+          </ConfiguredApolloProvider>
+        </LastBlockProvider>
+      </MathProvider>
+    </PolkadotJsProvider>
+  );
+};
