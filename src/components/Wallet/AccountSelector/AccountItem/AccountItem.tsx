4cc66941dcc34ff11ec88cf7868e983783e09eb3
Václav Slavík <vaclav.slavik@topmonks.com>
2022-01-13 17:10:30 +0100

    #41 | empty balances handling

--- src/components/Wallet/AccountSelector/AccountItem/AccountItem.tsx
+++ src/components/Wallet/AccountSelector/AccountItem/AccountItem.tsx
@@ -11,41 +11,45 @@ export interface AccountItemProps {
   active: boolean;
 }
 
-export const AccountItem = ({ account, onClick, active }: AccountItemProps) => (
-  <div
-    className={
-      'account-item ' +
-      classNames({
-        'account-item--active': active,
-      })
-    }
-    onClick={onClick}
-  >
-    <div className="d-flex flex-align-space">
-      <div className="account-item__heading">{account.name}</div>
-      <div>
-        <FormattedBalance
-          balance={account?.balances[0]}
-          unitStyle={UnitStyle.SHORT}
-          precision={1}
-        />
-      </div>
-    </div>
-    <div className="d-flex flex-column">
-      <div className="d-flex gap-2 my-1">
-        <Identicon value={account?.id} size={32} />
-        <div className="d-flex flex-column">
-          <div className="account-item__chain-name">Basilisk</div>
-          <div className="account-item__chain-address">{account?.id}</div>
+export const AccountItem = ({ account, onClick, active }: AccountItemProps) => {
+  return (
+    <div
+      className={
+        'account-item ' +
+        classNames({
+          'account-item--active': active,
+        })
+      }
+      onClick={onClick}
+    >
+      <div className="d-flex flex-align-space">
+        <div className="account-item__heading">{account.name}</div>
+        <div>
+          {account?.balances.map((balance, i) => (
+            <FormattedBalance
+              balance={balance}
+              unitStyle={UnitStyle.SHORT}
+              precision={1}
+            />
+          ))}
         </div>
       </div>
-      <div className="d-flex gap-2 my-1">
-        <Identicon value={account?.id} size={32} theme="polkadot" />
-        <div className="d-flex flex-column">
-          <div className="account-item__chain-name">Kusama</div>
-          <div className="account-item__chain-address">{account?.id}</div>
+      <div className="d-flex flex-column">
+        <div className="d-flex gap-2 my-1">
+          <Identicon value={account?.id} size={32} />
+          <div className="d-flex flex-column">
+            <div className="account-item__chain-name">Basilisk</div>
+            <div className="account-item__chain-address">{account?.id}</div>
+          </div>
+        </div>
+        <div className="d-flex gap-2 my-1">
+          <Identicon value={account?.id} size={32} theme="polkadot" />
+          <div className="d-flex flex-column">
+            <div className="account-item__chain-name">Kusama</div>
+            <div className="account-item__chain-address">{account?.id}</div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
