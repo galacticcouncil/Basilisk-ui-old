345bcc6a4b95e7169ec4a3c1aac27b82bcb1d358
Václav Slavík <vaclav.slavik@topmonks.com>
2022-01-13 10:22:01 +0100

    Basil page layout

--- src/containers/PageContainer.tsx
+++ src/containers/PageContainer.tsx
@@ -3,9 +3,16 @@ import { Wallet } from './Wallet';
 
 export const PageContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <Wallet />
+    <div className="container">
+      <div className="row my-5">
+        <div className="col-8">
+          <h1>Basilisk</h1>
+        </div>
+        <div className="col">
+          <Wallet />
+        </div>
+      </div>
       {children}
-    </>
+    </div>
   );
 };
