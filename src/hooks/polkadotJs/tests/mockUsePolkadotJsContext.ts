f0980c30474f72ac050cee2b73183a909962b7ee
dexterslabor <dexterslabor@users.noreply.github.com>
2022-01-27 17:00:28 +0000

    feat: add mocked polkadotJsContext for testing purposes

---
+++ src/hooks/polkadotJs/tests/mockUsePolkadotJsContext.ts
@@ -0,0 +1,31 @@
+import { ApiPromise } from '@polkadot/api';
+import { AccountId32 } from '@polkadot/types/interfaces';
+
+export const nativeAssetBalance = '10';
+export const nonNativeAssetBalance = '20';
+
+export const mockUsePolkadotJsContext = () => ({
+  apiInstance: {
+    query: {
+      system: {
+        account: jest.fn((arg: AccountId32 | string | Uint8Array) => {
+          return {
+            data: {
+              free: nativeAssetBalance,
+            },
+          };
+        }),
+      },
+      tokens: {
+        accounts: {
+          multi: jest.fn((arg: (unknown[] | unknown)[]) => {
+            return arg.map((arg) => {
+              return { free: nonNativeAssetBalance };
+            });
+          }),
+        },
+      },
+    },
+  } as unknown as ApiPromise,
+  loading: false,
+});
