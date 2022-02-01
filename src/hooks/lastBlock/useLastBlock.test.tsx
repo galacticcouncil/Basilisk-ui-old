9a63af5ee08f162bfa45142adde2779f7a5d50d7
janfabian <mail@janfabian.cz>
2022-02-01 10:24:17 +0100

    useLastBlock query

--- src/hooks/lastBlock/useLastBlock.test.tsx
+++ src/hooks/lastBlock/useLastBlock.test.tsx
@@ -1,10 +1,11 @@
 import TestRenderer, { act } from 'react-test-renderer';
 import waitForExpect from 'wait-for-expect';
-import { useLastBlock } from './useLastBlock';
+import { useLastBlock, __typename, id } from './useLastBlock';
 import { subscribeNewBlock } from './lib/subscribeNewBlock';
 import { getValidationData } from './lib/getValidationData';
-
-waitForExpect.defaults.interval = 1;
+import { setTimeout } from 'timers';
+waitForExpect.defaults.interval = 10;
+waitForExpect.defaults.timeout = 1000;
 
 jest.mock('../polkadotJs/usePolkadotJs', () => ({
   usePolkadotJsContext: () => {
@@ -12,7 +13,8 @@ jest.mock('../polkadotJs/usePolkadotJs', () => ({
     const {
       mockUsePolkadotJsContext,
     } = require('../polkadotJs/tests/mockUsePolkadotJsContext');
-    return mockUsePolkadotJsContext();
+
+    return mockUsePolkadotJsContext;
   },
 }));
 
@@ -44,15 +46,24 @@ describe('hooks/lastBlock/useLastBlock', () => {
     jest.resetModules();
   });
 
-  it('can resolve the query within the rendered component', async () => {
+  it('returns relay and parachain number', async () => {
+    const expectedLastBlock = {
+      __typename,
+      id,
+      relaychain: '10',
+      parachain: '100',
+    };
+
     const unsubscribe = jest.fn();
     (getValidationData as jest.Mock).mockImplementation(async () => {
-      return { relayParentNumber: 10 };
+      return { relayParentNumber: expectedLastBlock.relaychain };
     });
 
     (subscribeNewBlock as jest.Mock).mockImplementation(
       async (_apiInstance, callback) => {
-        setTimeout(() => callback({ block: { header: { number: 100 } } }), 0);
+        callback({
+          block: { header: { number: expectedLastBlock.parachain } },
+        });
 
         return unsubscribe;
       }
@@ -61,14 +72,76 @@ describe('hooks/lastBlock/useLastBlock', () => {
     render();
 
     await act(async () => {
-      await new Promise((res) => setTimeout(res, 1000));
+      await waitForExpect(() => {
+        expect(lastBlock).toMatchObject(expectedLastBlock);
+      });
+    });
 
-      console.log(lastBlock);
+    act(() => component.update(<Test />));
+    expect(unsubscribe).not.toBeCalled();
+    act(() => component.unmount());
+    expect(unsubscribe).toBeCalledTimes(1);
+    expect(getValidationData).toBeCalledTimes(1);
+    expect(getValidationData).toBeCalledWith(expect.any(Object), {
+      block: { header: { number: expectedLastBlock.parachain } },
+    });
+  });
 
-      // await waitForExpect(() => {
-      //     console.log(lastBlock)
-      //     expect(1).toEqual(1);
-      // });
+  it('returns relay and parachain number multiple times, once subscription returns new parachain number', async () => {
+    const expectedPrevLastBlock = {
+      __typename,
+      id,
+      relaychain: '10',
+      parachain: '100',
+    };
+
+    const expectedLastBlock = {
+      __typename,
+      id,
+      relaychain: '11',
+      parachain: '101',
+    };
+
+    const unsubscribe = jest.fn();
+    (getValidationData as jest.Mock).mockImplementationOnce(async () => {
+      return { relayParentNumber: expectedPrevLastBlock.relaychain };
+    });
+    (getValidationData as jest.Mock).mockImplementationOnce(async () => {
+      return { relayParentNumber: expectedLastBlock.relaychain };
+    });
+
+    (subscribeNewBlock as jest.Mock).mockImplementationOnce(
+      async (_apiInstance, callback) => {
+        callback({
+          block: { header: { number: expectedPrevLastBlock.parachain } },
+        });
+
+        setTimeout(() => {
+          callback({
+            block: { header: { number: expectedLastBlock.parachain } },
+          });
+        }, 100);
+        return unsubscribe;
+      }
+    );
+
+    render();
+
+    await act(async () => {
+      await waitForExpect(() => {
+        expect(lastBlock).toMatchObject(expectedPrevLastBlock);
+      });
+
+      await waitForExpect(() => {
+        expect(lastBlock).toMatchObject(expectedLastBlock);
+      });
+    });
+    expect(getValidationData).toBeCalledTimes(2);
+    expect(getValidationData).toBeCalledWith(expect.any(Object), {
+      block: { header: { number: expectedPrevLastBlock.parachain } },
+    });
+    expect(getValidationData).toBeCalledWith(expect.any(Object), {
+      block: { header: { number: expectedLastBlock.parachain } },
     });
   });
 });
