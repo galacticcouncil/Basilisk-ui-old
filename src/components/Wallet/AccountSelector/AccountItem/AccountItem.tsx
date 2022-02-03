2265f81c70908ba40f8656a69bf4dd45ab5cc3a1
Václav Slavík <vaclav.slavik@topmonks.com>
2022-02-03 13:50:06 +0100

    #41 | PR comment fix - account is always populated

--- src/components/Wallet/AccountSelector/AccountItem/AccountItem.tsx
+++ src/components/Wallet/AccountSelector/AccountItem/AccountItem.tsx
@@ -35,17 +35,17 @@ export const AccountItem = ({ account, onClick, active }: AccountItemProps) => {
       </div>
       <div className="d-flex flex-column">
         <div className="d-flex gap-2 my-1">
-          <Identicon value={account?.id} size={32} />
+          <Identicon value={account.id} size={32} />
           <div className="d-flex flex-column">
             <div className="account-item__chain-name">Basilisk</div>
-            <div className="account-item__chain-address">{account?.id}</div>
+            <div className="account-item__chain-address">{account.id}</div>
           </div>
         </div>
         <div className="d-flex gap-2 my-1">
-          <Identicon value={account?.id} size={32} theme="polkadot" />
+          <Identicon value={account.id} size={32} theme="polkadot" />
           <div className="d-flex flex-column">
             <div className="account-item__chain-name">Kusama</div>
-            <div className="account-item__chain-address">{account?.id}</div>
+            <div className="account-item__chain-address">{account.id}</div>
           </div>
         </div>
       </div>
