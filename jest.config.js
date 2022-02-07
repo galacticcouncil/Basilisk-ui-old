33df91269ad56b4da340930da99f896a1c093b95
Václav Slavík <vaclav.slavik@topmonks.com>
2022-02-07 11:03:12 +0100

    #41 | add transformIgnorePatterns for polkadot to jest.config.js file

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
+const esModules = ['@polkadot'].join('|');
+
 module.exports = {
   // preset: 'jest-playwright-preset',
   testRegex: './*\\.test\\.ts$',
   setupFilesAfterEnv: ['./src/setupTests.ts'],
   transform: {
     '^.+\\.(ts)$': 'ts-jest',
+    [`(${esModules}).+\\.js$`]: 'ts-jest',
   },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   setupFiles: ['<rootDir>/jest.env.js'],
   coverageReporters: [
     'clover',
