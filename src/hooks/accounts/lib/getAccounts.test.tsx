8667226b19def2470cab8318591293c70628d5fa
Václav Slavík <vaclav.slavik@topmonks.com>
2022-02-11 15:00:20 +0100

    #41 | getAccounts tests finished

--- src/hooks/accounts/lib/getAccounts.test.tsx
+++ src/hooks/accounts/lib/getAccounts.test.tsx
@@ -1,23 +1,14 @@
 import { Account } from '../../../generated/graphql';
 import { getAccounts } from './getAccounts';
 
-const mockAccounts = [
-  {
-    address: '5DwWSMW7kqBUU4Eo6s5dDvpJvKpXRgVZc24DUqqVXHH6UmFt',
-    meta: {
-      name: 'name',
-      source: 'source',
-    },
-  },
-];
+const mockWeb3Accounts = jest.fn();
+const mockWeb3Enable = jest.fn(async () => {
+  return true;
+});
 
 jest.mock('@polkadot/extension-dapp', () => ({
-  web3Accounts: async () => {
-    return mockAccounts;
-  },
-  web3Enable: async () => {
-    return true;
-  },
+  web3Accounts: () => mockWeb3Accounts(),
+  web3Enable: () => mockWeb3Enable(),
 }));
 
 describe('getAccounts', () => {
@@ -26,6 +17,15 @@ describe('getAccounts', () => {
   });
 
   it('can retrieve accounts', async () => {
+    mockWeb3Accounts.mockImplementation(() => [
+      {
+        address: '5DwWSMW7kqBUU4Eo6s5dDvpJvKpXRgVZc24DUqqVXHH6UmFt',
+        meta: {
+          name: 'name',
+          source: 'source',
+        },
+      },
+    ]);
     const accounts: Account[] = await getAccounts();
 
     expect(accounts).toEqual([
@@ -36,5 +36,16 @@ describe('getAccounts', () => {
         id: 'bXiUDcztNS6ZAgjy5zUzMHoUMbsz3hQ2Xh1sxyhvxKfoTHvK9',
       },
     ]);
+    expect(mockWeb3Accounts).toHaveBeenCalledTimes(1);
+    expect(mockWeb3Enable).toHaveBeenCalledTimes(1);
+  });
+
+  it('returns empty array when on accounts returned from polkadot', async () => {
+    mockWeb3Accounts.mockImplementation(() => []);
+    const accounts: Account[] = await getAccounts();
+
+    expect(accounts).toEqual([]);
+    expect(mockWeb3Accounts).toHaveBeenCalledTimes(1);
+    expect(mockWeb3Enable).toHaveBeenCalledTimes(1);
   });
 });
