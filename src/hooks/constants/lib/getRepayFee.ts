275f2826b58b9d7818603c8dbd6658da6959a37a
dexterslabor <dexterslabor@users.noreply.github.com>
2022-03-08 00:04:23 +0100

    feat: update tests and implementation

--- src/hooks/constants/lib/getRepayFee.ts
+++ src/hooks/constants/lib/getRepayFee.ts
@@ -1,7 +1,16 @@
 import '@polkadot/api-augment';
 import { ApiPromise } from '@polkadot/api';
 import { Fee } from '../../../generated/graphql';
-import { unwrapITupleU32ToFee } from './unwrapITupleU32ToFee';
 
-export const getRepayFee = (apiInstance: ApiPromise): Fee =>
-  unwrapITupleU32ToFee(apiInstance);
+export const repayFeeDataType = '(u32, u32)';
+
+export const getRepayFee = (apiInstance: ApiPromise): Fee => {
+  const repayFeeUntyped = apiInstance.consts.lbp.repayFee;
+
+  const repayFee = apiInstance.createType(repayFeeDataType, repayFeeUntyped);
+
+  return {
+    numerator: repayFee[0].toString(),
+    denominator: repayFee[1].toString(),
+  };
+};
