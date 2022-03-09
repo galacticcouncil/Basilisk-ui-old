c6f85161661cb94d9ad43e82880b2dbf88564ef2
timothydm42 <m.timothyd@gmail.com>
2022-03-09 09:29:44 -0700

    resolvers pr fixes for constants

--- src/hooks/constants/resolvers/query/constants/constants.tsx
+++ src/hooks/constants/resolvers/query/constants/constants.tsx
@@ -1,37 +1,14 @@
-import { useMemo } from 'react';
-import { ApiPromise } from '@polkadot/api';
 import { Constants } from '../../../../../generated/graphql';
-import { usePolkadotJsContext } from '../../../../polkadotJs/usePolkadotJs';
-import { withErrorHandler } from '../../../../apollo/withErrorHandler';
-import errors from '../../../../../errors';
 
 export const __typename: Constants['__typename'] = 'Constants';
-const withTypename = () => ({
+const id = 'constantsId';
+const withTypenameAndId = () => ({
   __typename,
+  id,
 });
 
-/**
- * Resolver for the `Constants` entity
- *
- * @param apiInstance ApiPromise
- */
-export const constantsQueryResolverFactory =
-  (apiInstance?: ApiPromise) => () => {
-    if (!apiInstance) throw Error(errors.apiInstanceNotInitialized);
-    return withTypename();
-  };
-
 export const useConstantsQueryResolver = () => {
-  const { apiInstance } = usePolkadotJsContext();
-
   return {
-    constants: useMemo(
-      () =>
-        withErrorHandler(
-          constantsQueryResolverFactory(apiInstance),
-          'constants'
-        ),
-      [apiInstance]
-    ),
+    constants: () => withTypenameAndId(),
   };
 };
--- src/hooks/constants/resolvers/query/lbpConstants/lbpConstants.tsx
+++ src/hooks/constants/resolvers/query/lbpConstants/lbpConstants.tsx
@@ -1,36 +1,42 @@
 import { useMemo } from 'react';
 import { ApiPromise } from '@polkadot/api';
-import { LbpConstants } from '../../../../../generated/graphql';
+import { LbpConstants, RepayFee } from '../../../../../generated/graphql';
 import { usePolkadotJsContext } from '../../../../polkadotJs/usePolkadotJs';
+import { getRepayFee } from '../../../lib/getRepayFee';
 import { withErrorHandler } from '../../../../apollo/withErrorHandler';
 import errors from '../../../../../errors';
 
-export const __typename: LbpConstants['__typename'] = 'LBPConstants';
-const withTypename = () => ({
-  __typename,
-});
+export const __typename_lbp_constants: LbpConstants['__typename'] =
+  'LBPConstants';
+export const __typename_repay_fee: RepayFee['__typename'] = 'RepayFee';
+const lbpConstantsId = 'lbpConstansId';
+const repayFeeId = 'repayFeeId';
 
-/**
- * sub-resolver for constants.lbp
- *
- * * @param apiInstance ApiPromise
- */
 export const lbpConstantsQueryResolverFactory =
   (apiInstance?: ApiPromise) =>
   (/** resolver arguments go here if needed in future */) => {
     if (!apiInstance) throw Error(errors.apiInstanceNotInitialized);
 
-    return withTypename();
+    return {
+      __typename: __typename_lbp_constants,
+      id: lbpConstantsId,
+      repayFee: {
+        __typename: __typename_repay_fee,
+        id: repayFeeId,
+        ...getRepayFee(apiInstance),
+      },
+    };
   };
 
 export const useLbpConstantsQueryResolver = () => {
   const { apiInstance } = usePolkadotJsContext();
+
   return {
     lbp: useMemo(
       () =>
         withErrorHandler(
           lbpConstantsQueryResolverFactory(apiInstance),
-          'lBPConstants'
+          'LBPConstants'
         ),
       [apiInstance]
     ),
