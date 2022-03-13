b6841c06359927f53cd0cd055fd45dac972f2b10
Matej Sima <matej.sima@gmail.com>
2022-03-14 00:40:44 +0100

    Add extension availability pseudo-polling

--- src/hooks/extension/lib/getExtension.tsx
+++ src/hooks/extension/lib/getExtension.tsx
@@ -1,9 +1,9 @@
 import { Extension } from '../../../generated/graphql';
+import promiseRetry from 'promise-retry';
 
 // id for cache normalization purposes, serves no other purpose
 const id = '0';
 // how long to wait before we check for the presence of the extension
-const waitForIsAvailableDuration = 100;
 
 // TODO: handle multiple extensions e.g. Talisman and polkadot.js
 // function that checks if an extension is available or not
@@ -11,17 +11,21 @@ export const getExtension = async (): Promise<Extension> => {
   // import the constant here to enable mocking in tests
   const { isAvailable }: Pick<Extension, 'isAvailable'> = await new Promise(
     (resolve) => {
-      setTimeout(async () => {
-        resolve({
-          /**
-           * We're not using isWeb3Available from `@polkadot/extension-dapp`,
-           * because it does not detect the `injectedWeb3` reliably.
-           */
-          // TODO: add polling of the extension availability,
-          // that will resolve as soon as this is available, or timeout after a certain time
-          isAvailable: !!(window as any).injectedWeb3?.['polkadot-js'],
-        });
-      }, waitForIsAvailableDuration);
+      promiseRetry(async (retry, attempt) => {
+        const isAvailable = !!(window as any).injectedWeb3?.['polkadot-js'];
+        console.log('getExtension attempt: #', attempt, isAvailable);
+        isAvailable
+          ? (
+            resolve({
+              /**
+               * We're not using isWeb3Available from `@polkadot/extension-dapp`,
+               * because it does not detect the `injectedWeb3` reliably.
+               */
+              isAvailable,
+            })
+          )
+          : retry('')
+      });
     }
   );
 
