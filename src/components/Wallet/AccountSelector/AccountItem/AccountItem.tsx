6d7403340efd3fe9024ac85dd37852a98c30cffc
Jakub Panik <jakub.panik@gmail.com>
2022-03-15 01:50:29 +0100

    wallet genesis hash handling

--- src/components/Wallet/AccountSelector/AccountItem/AccountItem.tsx
+++ src/components/Wallet/AccountSelector/AccountItem/AccountItem.tsx
@@ -8,6 +8,8 @@ import getBsxBalance from '../../../../misc/utils/getBsxBalance';
 import { trimAddress } from '../../Wallet';
 import { decode } from 'querystring';
 import { Maybe } from 'graphql/jsutils/Maybe';
+import registry, { RegistryEntry } from '@substrate/ss58-registry';
+import { knownGenesis } from '@polkadot/networks/genesis';
 
 export interface AccountItemProps {
   account: Account;
@@ -15,37 +17,44 @@ export interface AccountItemProps {
   active: boolean;
 }
 
-export const genesisHashToChain = (genesisHash: string) => {
-  const genesishHashPrefixes: Record<string, any> = {
-    '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe': {
-      name: 'Kusama',
-      prefix: 2
-    },
-    '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3': {
-      name: 'Polkadot',
-      prefix: 0
-    },
-    '0xbaf5aabe40646d11f0ee8abbdc64f4a4b7674925cba08e4a05ff9ebed6e2126b': {
-      name: 'Karura',
-      prefix: 8
-    },
-    '0xd2a620c27ec5cbc5621ff9a522689895074f7cca0d08e7134a7804e1a3ba86fc': {
-      name: 'HydraDX',
-      prefix: 63
+export const genesisHashToChain = (
+  genesisHash: Maybe<string>
+): RegistryEntry => {
+  // This is how Polkadot.js treats addresses by default if you don't chose chain
+  let chainInfo: RegistryEntry = {
+    prefix: 42,
+    network: 'substrate',
+    displayName: 'Substrate account',
+    symbols: [],
+    decimals: [],
+    standardAccount: '*25519',
+    website: 'https://substrate.io/',
+  };
+
+  // If we don't have genesis hash use default
+  if (!genesisHash) return chainInfo;
+
+  for (let chain in knownGenesis) {
+    if (knownGenesis[chain].includes(genesisHash)) {
+      const chainIndex = registry.findIndex((entry) => entry.network === chain);
+      if (chainIndex >= 0) {
+        chainInfo = registry[chainIndex];
+        break;
+      }
     }
   }
 
-  return genesishHashPrefixes[genesisHash];
-}
+  return chainInfo;
+};
 
 export const sourceToHuman = (source: Maybe<string>) => {
   switch (source) {
     case 'polkadot-js':
-      return 'Polkadot.js'
+      return 'Polkadot.js';
     case 'talisman':
-     return 'Talisman'
+      return 'Talisman';
   }
-}
+};
 
 export const AccountItem = ({ account, onClick, active }: AccountItemProps) => {
   return (
@@ -59,8 +68,12 @@ export const AccountItem = ({ account, onClick, active }: AccountItemProps) => {
     >
       <div className="account-item__heading">
         <div className="account-item__heading__left">
-          <div className="account-item__heading__left__name">{account.name}</div>
-          <div className="account-item__heading__left__source">{sourceToHuman(account.source)}</div>
+          <div className="account-item__heading__left__name">
+            {account.name}
+          </div>
+          <div className="account-item__heading__left__source">
+            {sourceToHuman(account.source)}
+          </div>
         </div>
         <div className="account-item__heading__balance">
           {<FormattedBalance balance={getBsxBalance(account)} />}
@@ -75,29 +88,37 @@ export const AccountItem = ({ account, onClick, active }: AccountItemProps) => {
           />
           <div className="account-item__chain-info">
             <div className="account-item__chain-name">Basilisk</div>
-            <div className="account-item__chain-address">{trimAddress(account.id, 24)}</div>
+            <div className="account-item__chain-address">
+              {trimAddress(account.id, 24)}
+            </div>
           </div>
         </div>
-        {account.genesisHash && genesisHashToChain(account.genesisHash)
-          ? (
-            <div className="account-item__address-entry">
-              <Identicon
-                className="account-item__identicon"
-                value={account.id}
-                size={32}
-                theme="polkadot"
-              />
-              <div className="account-item__chain-info">
-                <div className="account-item__chain-name">{genesisHashToChain(account.genesisHash)?.name}</div>
-                <div className="account-item__chain-address">{trimAddress(
-                  encodeAddress(decodeAddress(account.id), genesisHashToChain(account.genesisHash)?.prefix), 
+        {genesisHashToChain(account.genesisHash).network !== 'basilisk' ? (
+          <div className="account-item__address-entry">
+            <Identicon
+              className="account-item__identicon"
+              value={account.id}
+              size={32}
+              theme="polkadot"
+            />
+            <div className="account-item__chain-info">
+              <div className="account-item__chain-name">
+                {genesisHashToChain(account.genesisHash).displayName}
+              </div>
+              <div className="account-item__chain-address">
+                {trimAddress(
+                  encodeAddress(
+                    decodeAddress(account.id),
+                    genesisHashToChain(account.genesisHash)?.prefix
+                  ),
                   24
-                )}</div>
+                )}
               </div>
             </div>
-          )
-          : <></>
-        }
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
