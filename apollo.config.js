6f2b49bc1da5b8f3c3a394932d987d1076dc5a34
Matej Sima <matej.sima@gmail.com>
2022-03-24 10:39:50 +0100

    schema config updates

--- apollo.config.js
+++ apollo.config.js
@@ -1,9 +1,12 @@
+// https://github.com/apollographql/apollo-tooling/issues/812
 module.exports = {
     client: {
         service: {
             name: 'testnet',
             url: 'https://bsx-api-testnet.hydration.cloud/graphql',
-            excludes: ['**/**.test.tsx', '**/**.test.ts']
-        }
+            localSchemaFile: 'src/schema.graphql',
+            includes: ['src/**/*.ts{,x}']
+        },
+        excludes: ['**/**.test.tsx', '**/**.test.ts', 'src/schema.graphql', 'src/graphql.schema.json']
     }
 }
