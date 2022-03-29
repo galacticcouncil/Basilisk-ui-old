de2fb9459619fa13de5be17bafb289b510e21a05
Max Kravchuk <mckrava2@gmail.com>
2022-03-29 10:51:43 +0300

    feat: Add reporter workflow to issue comment (#117)
    * Add reporter workflow to issue comment
    
    * Fix github-script params
    
    * Fix reporter env vars
    
    * docs: update CI docs
    
    * docs: fix image in CI docs
    
    * refactor: refactor workflows titles and lables
    
    * Fix permission issue in script
    
    * refactor: Decorate steps names
    
    * refactor: Add comment into workflows
    
    * refactor: Remove redundant files
    
    * refactor: rename workflow
    
    * refactor: Rename job name
    
    * Refactor github scripts
    
    * docs: Update comments and docs in workflows
    
    * docs: Update Ci docs
    
    * Refactor steps params order
    
    * docs: Fix image in CI docs
    
    * dosc: Add comment into ggithub actions
    
    * docs: Update ci docs
    
    * docs: Improve CI docs
    
    * refactor: rename wf files, update wf comments
    
    * docs: Fix wf comments typos
    
    * docs: Refactor CI docs

---
+++ scripts/ci/github-script-src/utils/github-api.js
@@ -0,0 +1,93 @@
+function findCommentPredicate(inputs, comment) {
+  return (
+    (inputs.commentAuthor && comment.user
+      ? comment.user.login === inputs.commentAuthor
+      : true) &&
+    (inputs.bodyIncludes && comment.body
+      ? comment.body.includes(inputs.bodyIncludes)
+      : true)
+  );
+}
+
+async function publishIssueComment({
+  github,
+  context,
+  issueNumber,
+  owner,
+  repo,
+  commentBody,
+  existingIssueCommentId,
+}) {
+  try {
+    if (
+      !existingIssueCommentId ||
+      existingIssueCommentId === 'null' ||
+      !issueNumber
+    ) {
+      return await github.rest.issues.createComment({
+        issue_number: issueNumber,
+        owner,
+        repo,
+        body: commentBody,
+      });
+    } else {
+      return await github.rest.issues.updateComment({
+        owner,
+        repo,
+        comment_id: existingIssueCommentId,
+        body: commentBody,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return 1;
+  }
+}
+
+async function findIssueComment({
+  github,
+  context,
+  bodyIncludes,
+  issueNumber,
+  commentId = null,
+  commentAuthor = null,
+}) {
+  const [owner, repo] = context.payload.repository.full_name.split('/');
+  let comment = null;
+
+  if (!issueNumber || !owner || !repo) return null;
+
+  const parameters = {
+    owner: owner,
+    repo: repo,
+    issue_number: issueNumber,
+  };
+
+  if (commentId) {
+    comment = await github.rest.issues.getComment({
+      owner,
+      repo,
+      comment_id: commentId,
+    });
+
+    if (comment) return comment;
+  }
+
+  for await (const { data: comments } of github.paginate.iterator(
+    github.rest.issues.listComments,
+    parameters
+  )) {
+    // Search each page for the comment
+    comment = comments.find((comment) =>
+      findCommentPredicate({ commentAuthor, bodyIncludes }, comment)
+    );
+    if (comment) return comment;
+  }
+
+  return null;
+}
+
+module.exports = {
+  publishIssueComment,
+  findIssueComment,
+};
