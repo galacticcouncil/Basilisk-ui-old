6d82c1f44cff4bbe778544dd1105ebe9ece749b1
dexterslabor <dexterslabor@users.noreply.github.com>
2022-06-24 17:15:39 +0200

    feat: add router that computes path from asset A to B

---
+++ src/misc/router/types.tsx
@@ -0,0 +1,21 @@
+import { Asset, Pool } from '../../generated/graphql';
+
+export type Path = {
+  id: string; // address(es) of XYK Pool(s)
+  swaps: Swap[];
+  pools: Pool[];
+};
+
+export type Swap = {
+  id: string; // pool ID
+  assetIn: Asset;
+  assetOut: Asset;
+  //   swapAmount?: string;
+  //   limitReturnAmount?: string;
+  //   maxPrice?: string;
+};
+
+export enum SwapTypes {
+  SwapExactIn,
+  SwapExactOut,
+}
