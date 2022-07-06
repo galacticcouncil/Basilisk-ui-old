e8e28512e5e25a6ce88d3f54d19b9ada66389433
Matej Sima <matej.sima@gmail.com>
2022-07-06 14:39:32 +0200

    added tx fee estimate to transfer

--- src/pages/WalletPage/containers/WalletPage/TransferForm/TransferForm.tsx
+++ src/pages/WalletPage/containers/WalletPage/TransferForm/TransferForm.tsx
@@ -1,10 +1,14 @@
+import { useApolloClient } from '@apollo/client';
 import { watch } from 'fs';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { AssetBalanceInput } from '../../../../../components/Balance/AssetBalanceInput/AssetBalanceInput';
+import { FormattedBalance } from '../../../../../components/Balance/FormattedBalance/FormattedBalance';
 import Icon from '../../../../../components/Icon/Icon';
 import { Asset } from '../../../../../generated/graphql';
+import { estimateBalanceTransfer } from '../../../../../hooks/balances/resolvers/mutation/balanceTransfer';
 import { useTransferBalanceMutation } from '../../../../../hooks/balances/resolvers/useTransferMutation';
+import { usePolkadotJsContext } from '../../../../../hooks/polkadotJs/usePolkadotJs';
 import { Notification } from '../../../WalletPage';
 import './TransferForm.scss';
 
@@ -64,6 +68,30 @@ export const TransferForm = ({
   useEffect(() => {
     form.trigger('submit');
   }, [form.watch(['submit', 'amount', 'to', 'asset'])])
+
+  const [txFee, setTxFee] = useState<string>();
+  const { apiInstance, loading: apiInstanceLoading } = usePolkadotJsContext()
+  const client = useApolloClient();
+
+  useEffect(() => {
+    if (!apiInstance || apiInstanceLoading) return;
+    (async () => {
+      console.log('reestimating', {
+        from: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+        to: form.getValues('to') || '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+        currencyId: form.getValues('asset') || '0',
+        amount: form.getValues('amount') || '0'
+      });
+      const estimate = await estimateBalanceTransfer(client.cache, apiInstance, {
+        from: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+        to: form.getValues('to') || '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+        currencyId: form.getValues('asset') || '0',
+        amount: form.getValues('amount') || '0'
+      })
+      setTxFee(estimate.partialFee.toString());
+    })()
+  }, [apiInstance, apiInstanceLoading, client, form.watch(['amount', 'asset', 'to'])]);
+
   return (
     <div className="transfer-form modal-component-wrapper">
       <div className="transfer-form__content-wrapper">
@@ -100,6 +128,13 @@ export const TransferForm = ({
                 }
               })}
             />
+            Tx fee: {txFee
+              ? <FormattedBalance balance={{
+                assetId:'0',
+                balance: txFee
+              }}/> 
+              : <>-</>
+            }
           </form>
         </FormProvider>
       </div>
