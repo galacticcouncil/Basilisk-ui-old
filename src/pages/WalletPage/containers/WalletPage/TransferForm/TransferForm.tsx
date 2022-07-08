72cc20d5c60d88fa96746020d1ad93b0a1454a1f
Matej Holicky <10matejholicky@gmail.com>
2022-07-08 19:54:42 +0200

    feat: Add some styles for Wallet, still WIP

--- src/pages/WalletPage/containers/WalletPage/TransferForm/TransferForm.tsx
+++ src/pages/WalletPage/containers/WalletPage/TransferForm/TransferForm.tsx
@@ -17,12 +17,12 @@ export const TransferForm = ({
   closeModal,
   assetId = '0',
   setNotification,
-  assets
+  assets,
 }: {
-  closeModal: () => void,
-  assetId?: string,
-  setNotification: (notification: Notification) => void,
-  assets?: Asset[]
+  closeModal: () => void;
+  assetId?: string;
+  setNotification: (notification: Notification) => void;
+  assets?: Asset[];
 }) => {
   const modalContainerRef = useRef<HTMLDivElement | null>(null);
   const form = useForm({
@@ -31,116 +31,146 @@ export const TransferForm = ({
       asset: assetId,
       to: undefined,
       amount: undefined,
-      submit: undefined
+      submit: undefined,
     },
   });
 
   const [transferBalance] = useTransferBalanceMutation();
 
   const clearNotificationIntervalRef = useRef<any>();
-  const handleSubmit = useCallback((data: any) => {
-    // this is not ideal, but we want to show the pending status 
-    // which is hidden behind the modal currently
-    closeModal();
-    setNotification('pending');
-    transferBalance({
-      variables: {
-        currencyId: data.asset,
-        amount: data.amount,
-        to: data.to,
-      },
-      onCompleted: () => {
-        setNotification('success');
-        clearNotificationIntervalRef.current = setTimeout(() => {
-          setNotification('standby');
-        }, 4000);
-      },
-      onError: () => {
-        setNotification('failed');
-        clearNotificationIntervalRef.current = setTimeout(() => {
-          setNotification('standby');
-        }, 4000);
-      },
-    });
-  }, [closeModal, setNotification, transferBalance]);
+  const handleSubmit = useCallback(
+    (data: any) => {
+      // this is not ideal, but we want to show the pending status
+      // which is hidden behind the modal currently
+      closeModal();
+      setNotification('pending');
+      transferBalance({
+        variables: {
+          currencyId: data.asset,
+          amount: data.amount,
+          to: data.to,
+        },
+        onCompleted: () => {
+          setNotification('success');
+          clearNotificationIntervalRef.current = setTimeout(() => {
+            setNotification('standby');
+          }, 4000);
+        },
+        onError: () => {
+          setNotification('failed');
+          clearNotificationIntervalRef.current = setTimeout(() => {
+            setNotification('standby');
+          }, 4000);
+        },
+      });
+    },
+    [closeModal, setNotification, transferBalance]
+  );
 
   console.log('form state', form.formState);
 
   useEffect(() => {
     form.trigger('submit');
-  }, [form.watch(['submit', 'amount', 'to', 'asset'])])
+  }, [form.watch(['submit', 'amount', 'to', 'asset'])]);
 
   const [txFee, setTxFee] = useState<string>();
-  const { apiInstance, loading: apiInstanceLoading } = usePolkadotJsContext()
+  const { apiInstance, loading: apiInstanceLoading } = usePolkadotJsContext();
   const client = useApolloClient();
-  const { convertToFeePaymentAsset, feePaymentAsset } = useMultiFeePaymentConversionContext();
-
+  const { convertToFeePaymentAsset, feePaymentAsset } =
+    useMultiFeePaymentConversionContext();
 
   useEffect(() => {
     if (!apiInstance || apiInstanceLoading) return;
     (async () => {
       console.log('reestimating', {
         from: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-        to: form.getValues('to') || '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+        to:
+          form.getValues('to') ||
+          '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
         currencyId: form.getValues('asset') || '0',
-        amount: form.getValues('amount') || '0'
+        amount: form.getValues('amount') || '0',
       });
-      const estimate = await estimateBalanceTransfer(client.cache, apiInstance, {
-        from: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-        to: form.getValues('to') || '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
-        currencyId: form.getValues('asset') || '0',
-        amount: form.getValues('amount') || '0'
-      })
-      
-      setTxFee(
-        convertToFeePaymentAsset(estimate.partialFee.toString())
+      const estimate = await estimateBalanceTransfer(
+        client.cache,
+        apiInstance,
+        {
+          from: '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+          to:
+            form.getValues('to') ||
+            '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY',
+          currencyId: form.getValues('asset') || '0',
+          amount: form.getValues('amount') || '0',
+        }
       );
-    })()
-  }, [apiInstance, apiInstanceLoading, client, form.watch(['amount', 'asset', 'to'])]);
+      setTxFee(estimate.partialFee.toString());
+    })();
+  }, [
+    apiInstance,
+    apiInstanceLoading,
+    client,
+    form.watch(['amount', 'asset', 'to']),
+  ]);
 
   return (
     <div className="transfer-form modal-component-wrapper">
       <div className="transfer-form__content-wrapper">
         <div ref={modalContainerRef}></div>
-        <div className="modal-component-heading">
-          Transfer
+        <div className="transfer-form-heading">
+          <div className="transfer-form-title">Transfer</div>
           <div className="close-modal-btn" onClick={() => closeModal()}>
             <Icon name="Cancel" />
           </div>
         </div>
 
         <FormProvider {...form}>
-          <form onSubmit={form.handleSubmit(handleSubmit)}>
-            <label>To: </label>
-            {/* TODO: validate address */}
-            <input type="text" {...form.register('to', { required: true })} />
-            <AssetBalanceInput
-              modalContainerRef={modalContainerRef}
-              balanceInputName="amount"
-              assetInputName="asset"
-              assets={assets}
-            />
+          <form
+            className="transfer-form__transfer-form-parent"
+            onSubmit={form.handleSubmit(handleSubmit)}
+          >
+            <div className="transfer-form__transfer-form-asset-input-container">
+              <label>To: </label>
+              {/* TODO: validate address */}
+              <input
+                className="transfer-form__transfer-form-address-input"
+                type="text"
+                {...form.register('to', { required: true })}
+              />
+            </div>
 
-            Form state: {form.formState.isDirty ? 'dirty': 'clean'}, {form.formState.isValid ? 'valid' : 'invalid'}
-            
-            <input 
-              type="submit" 
-              className="submit-button" 
+            <div className="transfer-form__transfer-form-asset-input-container">
+              <label>Amount: </label>
+              <AssetBalanceInput
+                modalContainerRef={modalContainerRef}
+                balanceInputName="amount"
+                assetInputName="asset"
+                assets={assets}
+              />
+            </div>
+            {/* Form state: {form.formState.isDirty ? 'dirty': 'clean'}, {form.formState.isValid ? 'valid' : 'invalid'} */}
+            <div className="transfer-form__transfer-form-fee">
+              Tx fee:{' '}
+              {txFee ? (
+                <FormattedBalance
+                  balance={{
+                    assetId: '0',
+                    balance: txFee,
+                  }}
+                />
+              ) : (
+                <>-</>
+              )}
+            </div>
+            <input
+              type="submit"
+              className="transfer-form__submit-button"
               disabled={!form.formState.isDirty || !form.formState.isValid}
               {...form.register('submit', {
                 validate: {
                   asset: () => form.getValues('asset') !== undefined,
-                  amount: () => form.getValues('amount') !== undefined
-                }
+                  amount: () => form.getValues('amount') !== undefined,
+                },
               })}
             />
-            Tx fee: {txFee
-              ? <FormattedBalance balance={{
-                assetId: feePaymentAsset || '0',
-                balance: txFee
-              }}/> 
-              : <>-</>
-            }
           </form>
         </FormProvider>
       </div>
--- src/pages/WalletPage/containers/WalletPage/VestingClaim/VestingClaim.tsx
+++ src/pages/WalletPage/containers/WalletPage/VestingClaim/VestingClaim.tsx
@@ -1,82 +1,116 @@
-import { useApolloClient } from "@apollo/client";
-import BigNumber from "bignumber.js";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { FormattedBalance } from "../../../../../components/Balance/FormattedBalance/FormattedBalance";
-import { useMultiFeePaymentConversionContext } from "../../../../../containers/MultiProvider";
-import { Maybe, Vesting } from "../../../../../generated/graphql";
-import { fromPrecision12 } from "../../../../../hooks/math/useFromPrecision";
-import { usePolkadotJsContext } from "../../../../../hooks/polkadotJs/usePolkadotJs";
-import { useClaimVestedAmountMutation } from "../../../../../hooks/vesting/useClaimVestedAmountMutation";
-import { estimateClaimVesting } from "../../../../../hooks/vesting/useVestingMutationResolvers";
-import { Notification } from "../../../WalletPage";
+import { useApolloClient } from '@apollo/client';
+import BigNumber from 'bignumber.js';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { FormattedBalance } from '../../../../../components/Balance/FormattedBalance/FormattedBalance';
+import { useMultiFeePaymentConversionContext } from '../../../../../containers/MultiProvider';
+import { Maybe, Vesting } from '../../../../../generated/graphql';
+import { fromPrecision12 } from '../../../../../hooks/math/useFromPrecision';
+import { usePolkadotJsContext } from '../../../../../hooks/polkadotJs/usePolkadotJs';
+import { useClaimVestedAmountMutation } from '../../../../../hooks/vesting/useClaimVestedAmountMutation';
+import { estimateClaimVesting } from '../../../../../hooks/vesting/useVestingMutationResolvers';
+import { Notification } from '../../../WalletPage';
+import './VestingClaim.scss';
 
-export const VestingClaim = ({ vesting, setNotification }: {
-    vesting?: Maybe<Vesting | undefined>,
-    setNotification: (notification: Notification) => void
-  }) => {
-    const isVestingAvailable = useMemo(() => {
-      return vesting?.originalLockBalance && new BigNumber(vesting?.originalLockBalance).gt('0');
-    }, [vesting]);
-    const clearNotificationIntervalRef = useRef<any>();
-    const [claimVestedAmount] = useClaimVestedAmountMutation({
-      onCompleted: () => {
-        setNotification('success');
-        clearNotificationIntervalRef.current = setTimeout(() => {
-          setNotification('standby');
-        }, 4000);
-      },
-      onError: () => {
-        setNotification('failed');
-        clearNotificationIntervalRef.current = setTimeout(() => {
-          setNotification('standby');
-        }, 4000);
-      },
-    });
-  
-    // TODO: run mutation with confirmation
-    const handleClaimClick = useCallback(() => {
-      setNotification('pending');
-      claimVestedAmount()
-    }, []);
+export const VestingClaim = ({
+  vesting,
+  setNotification,
+}: {
+  vesting?: Maybe<Vesting | undefined>;
+  setNotification: (notification: Notification) => void;
+}) => {
+  const isVestingAvailable = useMemo(() => {
+    return (
+      vesting?.originalLockBalance &&
+      new BigNumber(vesting?.originalLockBalance).gt('0')
+    );
+  }, [vesting]);
+  const clearNotificationIntervalRef = useRef<any>();
+  const [claimVestedAmount] = useClaimVestedAmountMutation({
+    onCompleted: () => {
+      setNotification('success');
+      clearNotificationIntervalRef.current = setTimeout(() => {
+        setNotification('standby');
+      }, 4000);
+    },
+    onError: () => {
+      setNotification('failed');
+      clearNotificationIntervalRef.current = setTimeout(() => {
+        setNotification('standby');
+      }, 4000);
+    },
+  });
 
-    const { apiInstance, loading: apiInstanceLoading } = usePolkadotJsContext();
-    const client = useApolloClient();
-    const { feePaymentAsset, convertToFeePaymentAsset } = useMultiFeePaymentConversionContext()
+  // TODO: run mutation with confirmation
+  const handleClaimClick = useCallback(() => {
+    setNotification('pending');
+    claimVestedAmount();
+  }, []);
 
-    const [txFee, setTxFee] = useState<string>();
-    useEffect(() => {
-      if (!apiInstance || apiInstanceLoading) return;
-      (async () => {
-        const txFee = await estimateClaimVesting(client.cache as any, apiInstance, {});
-        console.log('claim tx fee', convertToFeePaymentAsset(txFee.partialFee.toString()));
-        setTxFee(convertToFeePaymentAsset(txFee.partialFee.toString()));
-      })()
-    }, [apiInstance, apiInstanceLoading, estimateClaimVesting, client, convertToFeePaymentAsset])
-  
-    return <>
-      <h2>Vesting</h2>
-      {isVestingAvailable
-        ? (
-          <>
-            <p>Claimable: {fromPrecision12(vesting?.claimableAmount)} BSX</p>
-            <p>Original vesting (TODO: fix calc): {fromPrecision12(vesting?.originalLockBalance)} BSX</p>
-            <p>Remaining vesting: {fromPrecision12(vesting?.lockedVestingBalance)} BSX</p>
-            <button onClick={() => handleClaimClick()}>claim</button>
-            Tx fee: {txFee
-              ? <FormattedBalance balance={{
-                assetId: feePaymentAsset || '0',
-                balance: txFee
-              }}/> 
-              : <>-</>
-            }
-          </>
-        )
-        : (
-          <>
-            No vesting available
-          </>
-        )
-      }
-      
-    </>
-  }
+  const { apiInstance, loading: apiInstanceLoading } = usePolkadotJsContext();
+  const client = useApolloClient();
+  const { feePaymentAsset, convertToFeePaymentAsset } =
+    useMultiFeePaymentConversionContext();
+
+  const [txFee, setTxFee] = useState<string>();
+  useEffect(() => {
+    if (!apiInstance || apiInstanceLoading) return;
+    (async () => {
+      const txFee = await estimateClaimVesting(
+        client.cache as any,
+        apiInstance,
+        {}
+      );
+      console.log(
+        'claim tx fee',
+        convertToFeePaymentAsset(txFee.partialFee.toString())
+      );
+      setTxFee(convertToFeePaymentAsset(txFee.partialFee.toString()));
+    })();
+  }, [
+    apiInstance,
+    apiInstanceLoading,
+    estimateClaimVesting,
+    client,
+    convertToFeePaymentAsset,
+  ]);
+
+  return (
+    <div className="vesting-claim">
+      <h2 className="vesting-claim__title">Vesting</h2>
+      {isVestingAvailable ? (
+        <div className="vesting-claim-wrapper">
+          <div>Claimable: {fromPrecision12(vesting?.claimableAmount)} BSX</div>
+          <div>
+            Original vesting (TODO: fix calc):{' '}
+            {fromPrecision12(vesting?.originalLockBalance)} BSX
+          </div>
+          <div>
+            Remaining vesting: {fromPrecision12(vesting?.lockedVestingBalance)}{' '}
+            BSX
+          </div>
+          Tx fee:{' '}
+          <div className="vesting-claim__fee">
+            {txFee ? (
+              <FormattedBalance
+                balance={{
+                  assetId: feePaymentAsset || '0',
+                  balance: txFee,
+                }}
+              />
+            ) : (
+              <>-</>
+            )}
+          </div>
+          <button
+            className="vesting-claim-button"
+            onClick={() => handleClaimClick()}
+          >
+            <div className="vesting-claim-button__label">Claim</div>
+          </button>
+        </div>
+      ) : (
+        <>No vesting available</>
+      )}
+    </div>
+  );
+};
